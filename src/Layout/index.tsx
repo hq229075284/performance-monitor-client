270c65b1a8f7bc327ac00aa3e0067f6f8c1d5c8f
hanqing <hanq@qq.com>
2021-07-05 15:29:48 +0800

    update

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,24 +1,27 @@
 import type {FC} from 'react';
+import type {RouteComponentProps} from 'react-router';
 import React  from 'react';
 import SideBar from './components/SideBar';
 import './index.scss';
 // import routeItems from 'src/router/routeItem';
 // import type {RouteItem} from 'src/router/routeItem';
-import Router from 'src/router';
+import {Route} from 'react-router-dom';
 
 import {Layout as AntdLayout} from 'antd';
 const {Sider, Content} = AntdLayout;
 
 
 // class Layouts extends React.Component<LayoutProps> {
-const Layout: FC = props => {
+const Layout: FC<RouteComponentProps> = (props) => {
+  console.log(props.match.path);
   return (
     <AntdLayout className='dc-app-layout'>
       <Sider width="280" className='dc-sidebar-container'>
         <SideBar></SideBar>
       </Sider>
       <Content className='dc-main-container'>
-        <Router></Router>
+        <Route path={props.match.path + '/1'} component={()=><div>1</div>}/>
+        <Route path={props.match.path + '/2'} component={()=><div>2</div>}/>
       </Content>
     </AntdLayout>
   );
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -5,7 +5,7 @@ import {Router, Redirect, Route, Switch} from 'react-router-dom';
 import Config from 'src/config';
 import history from './history';
 import Login from 'src/views/Login';
-import Console from 'src/views/Console';
+// import Console from 'src/views/Console';
 // import type {RouteItem} from './routeItem';
 import routeItem from './routeItem';
 // import {isEmpty} from 'src/utils';
@@ -20,9 +20,7 @@ const Routes = () => {
       <Switch>
         <Redirect from="/" to={{pathname: exactPath}} exact />
         <Route path={Config.LOGIN_PATH} component={Login} exact />
-        <Route path='/' component={Layout}>
-          <Route path='/console' component={Console} />
-        </Route>
+        <Route path='/layout' component={Layout}></Route>
       </Switch>
     </Router>
   );
